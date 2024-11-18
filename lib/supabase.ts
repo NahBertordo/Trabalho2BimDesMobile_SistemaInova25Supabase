0678db6f4dd0edbb37eca3f20c97087c57abb816
NahBertordo <40553388+NahBertordo@users.noreply.github.com>
2024-11-18 16:29:58 -0300

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,8 +1,8 @@
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = "https://dccnfglyslddwxnxetpx.supabase.co"
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRjY25mZ2x5c2xkZHd4bnhldHB4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzE3MDU0NTAsImV4cCI6MjA0NzI4MTQ1MH0.6b96RMaYyhlis9nZFg1byp-zlorjTIAQq5nXRNkYwmo"
+const supabaseUrl = ""
+const supabaseAnonKey = ""
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
